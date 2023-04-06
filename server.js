460002945c8dd0b35e33d1acc60ddb38c559bc53
Douglas Leal <douglas.leal@live.com>
2023-04-06 15:25:36 -0300

    Adicionando o EJS

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import ejs from "ejs";
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render("index")
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Servidor rodando na porta ${3000}...`)
 })
